44374bcc51ec536a3da1beedf5053b741f34b970
Louis-Philippe <louisphilstarno@hotmail.ca>
2022-02-08 12:10:20 -0500

    This version, edited by LP, has working hazard light and blinker functionality (input and output).

--- AccessoryBoard/AccessoryBoard.ino
+++ AccessoryBoard/AccessoryBoard.ino
@@ -6,25 +6,41 @@
 
 // constants won't change. They're used here to 
 // set pin numbers:
-const int leftInput = PUSH1; 
-const int rightInput = PUSH2; 
+//const int leftInput = PUSH1; 
+//const int rightInput = PUSH2;
 
-const int leftInputWheel = 34; // 34; // p4.3 on board // 
+const int leftInputWheel = 34;  //34 p4.3 on board // 
 const int rightInputWheel = 35; // 35; // p1.2 on board // 
 
-const int hazardInput =  31; // p8.2 on board // PUSH2; //
+const int hazardInput = 31; //31 ; p8.2 on board // PUSH2; //
 const int leftOut =   39; // p2.4 on board //  GREEN_LED; // 
 const int rightOut =   40; // p2.5 on board // RED_LED; // 
 
 // variables to read high/low input
-int leftState = 0;
-int rightState = 0;
+//int leftState = 0;
+//int rightState = 0;
 
 int leftStateWheel = 0;
 int rightStateWheel = 0;
-
+int ledState;
+int active = 0;
 int hazardState = 0;
 
+unsigned long previousMillis = 0;
+unsigned long currentMillis;
+const long interval = 300;
+void Blink(int pin){
+  if(millis() - previousMillis >= interval){
+    previousMillis = millis();
+    if (ledState == LOW){
+        ledState = HIGH;
+    }
+    else {
+      ledState = LOW;
+    }
+  }
+  digitalWrite(pin,ledState);
+}
 void setup() {
 
   // pin outputs
@@ -32,63 +48,63 @@ void setup() {
   pinMode(rightOut,OUTPUT);
 
   // pin inputs
-  pinMode(leftInput,INPUT_PULLUP);
-  pinMode(rightInput, INPUT_PULLUP);
+  //pinMode(leftInput,INPUT_PULLUP);
+  //pinMode(rightInput, INPUT_PULLUP);
 
   pinMode(leftInputWheel, INPUT);
   pinMode(rightInputWheel, INPUT);
 
   
   pinMode(hazardInput, INPUT);
-   
+ // Serial.begin(9600);
 }
 
 void loop(){
   
-  leftState  = digitalRead(leftInput);
-  rightState = digitalRead(rightInput);
+  //leftState  = digitalRead(leftInput);
+  //rightState = digitalRead(rightInput);
+  
+  leftStateWheel = LOW; //digitalRead(leftInputWheel);
+  rightStateWheel = LOW; //digitalRead(leftInputWheel);
+  hazardState = LOW; //digitalRead(hazardInput);
 
-  leftStateWheel = digitalRead(leftInputWheel);
-  rightStateWheel = digitalRead(leftInputWheel);
-  
-  hazardState = digitalRead(hazardInput);
-  
+  if (digitalRead(leftInputWheel)== LOW && digitalRead(rightInputWheel)== LOW && digitalRead(hazardInput)== LOW) //blinker not active
+    ledState = LOW;
+ 
+  if (digitalRead(hazardInput) == HIGH){
+    hazardState = HIGH;
+   // Serial.println("Left");
+  }
+  else if (digitalRead(rightInputWheel) == HIGH){
+    rightStateWheel = HIGH;
+   // Serial.println("Right");
+  }
+  else if (digitalRead(leftInputWheel) == HIGH){
+    leftStateWheel = HIGH;
+   // Serial.println("Hazard");
+  }
 // Hazard Lights - only pull low if both leftState and rightState are low as well
-  if (hazardState == LOW && leftState == HIGH && rightState  == HIGH){
-    digitalWrite(leftOut,LOW);
-    digitalWrite(rightOut,LOW);
-    return;
-  }else if (hazardState == HIGH){
+  if (hazardState == HIGH){
     // blink logic
-    digitalWrite(leftOut,HIGH);
-    digitalWrite(rightOut,HIGH);
-    delay(300);
-    digitalWrite(leftOut,LOW);
-    digitalWrite(rightOut,LOW);
-    delay(300);
-    return;
-  }
-
-  // Left Signal
-  if (leftState == HIGH || leftStateWheel == LOW){
-    digitalWrite(leftOut,LOW);
-  }else if (leftState == LOW || leftStateWheel == HIGH){
+    Blink(rightOut);
+    Blink(leftOut);
+   // Serial.println("Haz blink");
+    //return;
+  } else {
+    if (leftStateWheel == HIGH){ //Turn left signal on
     // blink logic
-    digitalWrite(leftOut,HIGH);
-    delay(300);
-    digitalWrite(leftOut,LOW);
-    delay(300);
+    Blink(leftOut);
+    //Serial.println("Left blink");
+    //return;
+  } else { //Turn left signal off
+    digitalWrite(leftOut, LOW);
   }
-
-  // Right Signal
-  if (rightState == HIGH || rightStateWheel == LOW){
-    digitalWrite(rightOut,LOW);
-  }else if (rightState == LOW || rightStateWheel == HIGH){
-    // blink logic
-    digitalWrite(rightOut,HIGH);
-    delay(300);
-    digitalWrite(rightOut,LOW);
-    delay(300);
+      if (rightStateWheel == HIGH){ //Turn right signal on
+      // blink logic
+      Blink(rightOut);
+      //Serial.println("Right blink");
+    } else { //Turn right signal off
+      digitalWrite(rightOut, LOW);
+    }
   }
-  
 }
