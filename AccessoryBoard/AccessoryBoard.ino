dd8757da40cdbf12f488f5d612f09f39ca64b5d0
strem078 <strem078@uottawa.ca>
2022-02-12 20:24:06 -0500

    Added comments and put loop code into own functions. Modified pin names to the energia compileable P#_# format.

--- AccessoryBoard/AccessoryBoard.ino
+++ AccessoryBoard/AccessoryBoard.ino
@@ -1,47 +1,37 @@
 // Authors:
-// Omar Hayat, Kian Bazarjani
+// Louis-Philippe St-Arnaud, Omar Hayat, Kian Bazarjani
 //
-// Hello Omar
 // DESCRIPTION:
 //    Accessory component functionality for prototype vehicle to be used in Shell Eco-Marathon Competition
+//    This code is for Texas Instruments MSP-EXP430F5529LP boards, compiled in energia.
 
-// constants won't change. They're used here to 
-// set pin numbers:
-//const int leftInput = PUSH1; 
-//const int rightInput = PUSH2;
+// These constants won't change. They're used here to 
+// set pin numbers (see accessory board documentation):
 
-const int leftInputWheel = 34;  //34 p4.3 on board // 
-const int rightInputWheel = 35; // 35; // p1.2 on board // 
+const int leftInputWheel = P4_3; // Flasher input on steering, left
+const int rightInputWheel = P1_2; // Flasher input on steering, right
 
-const int hazardInput = 31; //31 ; p8.2 on board // PUSH2; //
-const int leftOut =   39; // p2.4 on board //  GREEN_LED; // 
-const int rightOut =   40; // p2.5 on board // RED_LED; // 
+const int hazardInput = P8_2; // Hazards button on steering
+const int leftOut = P2_4; // Output pin for left flashers
+const int rightOut = P2_5; // Output pin for right flashers
 
-// variables to read high/low input
-//int leftState = 0;
-//int rightState = 0;
-
-int leftStateWheel = 0;
-int rightStateWheel = 0;
-int ledState;
-int active = 0;
-int hazardState = 0;
+// State variables
+int leftStateWheel = LOW;
+int rightStateWheel = LOW;
+int ledState = LOW; // Holds the current state of the blinkers
+int hazardState = LOW; 
 
 unsigned long previousMillis = 0;
 unsigned long currentMillis;
-const long interval = 300;
-void Blink(int pin){
-  if(millis() - previousMillis >= interval){
-    previousMillis = millis();
-    if (ledState == LOW){
-        ledState = HIGH;
-    }
-    else {
-      ledState = LOW;
-    }
-  }
-  digitalWrite(pin,ledState);
-}
+const long interval = 300; // Time between blinks (milliseconds)
+
+// Function prototypes for the blinking
+void blink(int);
+void updateblinkers();
+
+////////////////////////////////////////////
+//             SETUP FUNCTION             //
+////////////////////////////////////////////
 void setup() {
 
   // pin outputs
@@ -49,63 +39,70 @@ void setup() {
   pinMode(rightOut,OUTPUT);
 
   // pin inputs
-  //pinMode(leftInput,INPUT_PULLUP);
-  //pinMode(rightInput, INPUT_PULLUP);
-
   pinMode(leftInputWheel, INPUT);
   pinMode(rightInputWheel, INPUT);
-
-  
   pinMode(hazardInput, INPUT);
- // Serial.begin(9600);
+  
+  // Serial.begin(9600);
 }
 
+////////////////////////////////////////////
+//            LOOPING FUNCTION            //
+////////////////////////////////////////////
 void loop(){
   
-  //leftState  = digitalRead(leftInput);
-  //rightState = digitalRead(rightInput);
-  
-  leftStateWheel = LOW; //digitalRead(leftInputWheel);
-  rightStateWheel = LOW; //digitalRead(leftInputWheel);
-  hazardState = LOW; //digitalRead(hazardInput);
+  updateBlinkers();
+
+}
+
+// This function reads the blinker state and 
+void updateBlinkers() {
+
+  leftStateWheel = digitalRead(leftInputWheel);
+  rightStateWheel = digitalRead(rightInputWheel);
+  hazardState = digitalRead(hazardInput);
 
   if (digitalRead(leftInputWheel)== LOW && digitalRead(rightInputWheel)== LOW && digitalRead(hazardInput)== LOW) //blinker not active
     ledState = LOW;
- 
-  if (digitalRead(hazardInput) == HIGH){
-    hazardState = HIGH;
-   // Serial.println("Left");
-  }
-  else if (digitalRead(rightInputWheel) == HIGH){
-    rightStateWheel = HIGH;
-   // Serial.println("Right");
-  }
-  else if (digitalRead(leftInputWheel) == HIGH){
-    leftStateWheel = HIGH;
-   // Serial.println("Hazard");
-  }
-// Hazard Lights - only pull low if both leftState and rightState are low as well
+
+  // Hazard Lights - only pull low if both leftState and rightState are low as well
   if (hazardState == HIGH){
-    // blink logic
-    Blink(rightOut);
-    Blink(leftOut);
-   // Serial.println("Haz blink");
+    // Blink logic
+    // millis makes these two
+    // run seemingly simultaneously
+    blink(rightOut);
+    blink(leftOut);
     //return;
   } else {
-    if (leftStateWheel == HIGH){ //Turn left signal on
-    // blink logic
-    Blink(leftOut);
-    //Serial.println("Left blink");
-    //return;
-  } else { //Turn left signal off
-    digitalWrite(leftOut, LOW);
-  }
-      if (rightStateWheel == HIGH){ //Turn right signal on
+    if (leftStateWheel == HIGH) { //Turn left signal on
       // blink logic
-      Blink(rightOut);
+      blink(leftOut);
+      //Serial.println("Left blink");
+      //return;
+    } else { //Turn left signal off
+      digitalWrite(leftOut, LOW);
+    }
+
+    if (rightStateWheel == HIGH) { //Turn right signal on
+      // blink logic
+      blink(rightOut);
       //Serial.println("Right blink");
     } else { //Turn right signal off
       digitalWrite(rightOut, LOW);
     }
   }
+
+}
+
+// This function does the actual blinking
+void blink(int pin){
+  if(millis() - previousMillis >= interval){
+    previousMillis = millis();
+    if (ledState == LOW){
+        ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+  }
+  digitalWrite(pin,ledState);
 }
